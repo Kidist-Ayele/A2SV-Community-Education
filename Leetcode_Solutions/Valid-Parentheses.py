24b2957adca52596cafd0e327b9e40324787de4a
Kidist_Ayele <kidistayele37@gmail.com>
2024-05-29 22:40:32 +0300

    Stack

---
+++ Leetcode_Solutions/Valid-Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToopen = { ")" : "(", "]" : "]", "}" : "{"}
+
+        for char in s:
+            if char in closeToopen:
+                if stack and stack[-1] == closeToopen[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return True if not stack else False
+           
+
+        
